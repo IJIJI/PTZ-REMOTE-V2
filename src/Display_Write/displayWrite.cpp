383ad17596660e768d5011438d28c02357968547
IJssel <54723855+IJIJI@users.noreply.github.com>
2020-11-16 18:38:51 +0100

    Started display library

---
+++ src/Display_Write/displayWrite.cpp
@@ -0,0 +1,14 @@
+/*
+  displayWrite.h - Display library for the PTZ-Remote - implementation
+  Copyright (c) 2020 IJssel Koster.  All right reserved.
+*/
+
+#include "displayWrite.h"
+
+displayWrite::displayWrite(/* args */)
+{
+}
+
+displayWrite::~displayWrite()
+{
+}
---
+++ src/Display_Write/displayWrite.h
@@ -0,0 +1,19 @@
+/*
+  displayWrite.h - Display library for the PTZ-Remote - description
+  Copyright (c) 2020 IJssel Koster.  All right reserved.
+*/
+
+#ifndef Test_h
+#define Test_h
+
+class displayWrite
+{
+  public:
+    displayWrite(/* args */);
+
+  private:
+    /* data */
+};
+
+
+#endif
