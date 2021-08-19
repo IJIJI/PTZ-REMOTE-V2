9682c48dd2d550be0ac81d5fd02fc4f40514e1d5
IJssel <54723855+IJIJI@users.noreply.github.com>
2021-08-19 16:49:26 +0200

    added commands with explination

---
+++ commands.h
@@ -0,0 +1,54 @@
+
+
+
+
+const String commandNames[] = {"null", "joyUpdate", "focus", "goToCoords", "writePos", "writePosCoords", "callPos", "setCoords", "callError", "setLimit"};
+
+const byte joyUpdateCommand       =   B00000001;
+const byte focusCommand           =   B00000010;  //! WIP
+const byte goToCoordsCommand      =   B00000011;  //! WIP
+const byte writePosCommand        =   B00000100;
+const byte writePosCoordsCommand  =   B00000101;  //! WIP
+const byte callPosCommand         =   B00000110;
+const byte setCoordsCommand       =   B00000111;  //! WIP
+const byte callErrorCommand       =   B00001000;
+const byte setLimitCommand        =   B00001001;  //! WIP
+
+
+const byte terminatorByte         =   0xFF;
+
+
+
+//? Command length = 15 bytes
+
+// byte 0     =   camNum
+// byte 1     =   command identifier
+// byte 2-12  =   data
+// byte 13    =   checkSum 
+// byte 14    =   terminatorByte
+
+
+//? Checksum
+// Sum off all bytes excluding startByte mod 256.
+// checkSum = (sum of bytes)%256
+
+
+
+enum errorType {
+  unknown,
+  emergenyStop,
+  collision
+};
+
+errorType errorTypes;
+
+
+
+enum limitType {
+  xMin,
+  xMax,
+  yMin,
+  yMax
+};
+
+limitType limitTypes;
